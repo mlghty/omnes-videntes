cd7edd760c42da53d1f16b12129d44ea12ed5ca2
the-mighty-fall <33680224+the-mighty-fall@users.noreply.github.com>
2020-11-14 18:47:57 -0600

    Fixed some indentations

--- kids/utils/windows.py
+++ kids/utils/windows.py
@@ -6,9 +6,7 @@ import win32process #old
 
 def get_all_windows():
     windows = []
-
     win32gui.EnumWindows(construct_windows_list, windows)
-
     return windows
 
 
@@ -16,16 +14,12 @@ def construct_windows_list(hwnd, strings):
     if win32gui.IsWindowVisible(hwnd):
         title = win32gui.GetWindowText(hwnd)
         pid = get_window_pid(title)
-
         left, top, right, bottom = win32gui.GetWindowRect(hwnd)
-
         if title and right - left and bottom - top:
             strings.append({"title": title, "pid": pid})
-
     return True
 
 def get_window_pid(title): # Returns windos PID
     hwnd = win32gui.FindWindow(None, title)
     _, pid = win32process.GetWindowThreadProcessId(hwnd)
-
     return pid
