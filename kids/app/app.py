6dd27befcf60cdce402ef797b572611c56889143
the-mighty-fall <33680224+the-mighty-fall@users.noreply.github.com>
2020-11-25 21:38:50 -0600

    gui update

--- kids/app/app.py
+++ kids/app/app.py
@@ -1,51 +1,72 @@
-import tkinter
-import time
-
+import tkinter as tk
 from tkinter import ttk
+import time
 
 from kids.utils import get_all_windows,  get_windows_processes
 from kids.gui import create_process_tree
 
-processes = get_windows_processes(get_all_windows())
-windows = get_all_windows()
+# processes = get_windows_processes(get_all_windows()) //  case 1
+# windows = get_all_windows() // case 2
 
-class App:
+class app:
     def __init__(self):
-        """ Creates a new window"""
-        self.root = tkinter.Tk()
-        self.root.title("Spy Kids 5")
-        self.root.geometry("600x600")
-        self._create_widgets()
+        self.root = tk.Tk()
+        self.root.title("Spy Kids 5") # Change name eventually
+        self.root.geometry("500x700") # Change window size or fix the expand fill for tab1
+        self.notebook()
+        #self._create_widgets()
 
-    def _create_widgets(self):
-        self.notebook = ttk.Notebook(self.root)
-
+    def notebook(self):
+        self.tabControl = ttk.Notebook(self.root)
         processes = get_windows_processes(get_all_windows())
 
-        self.tree = create_process_tree(self.notebook, processes)
+        # Style
+        noteStyler = ttk.Style()
+        noteStyler.configure("TNotebook", background='white', borderwidth=0)
+        noteStyler.configure("TNotebook.Tab", background='white')
+        noteStyler.configure("TFrame", background='white')
+        
+        # Tabs
+        self.tab1 = create_process_tree(self.tabControl, processes)
+        self.tab2 = ttk.Frame(self.tabControl)
 
-        self.notebook.add(self.tree, text="Apps")
-        self.notebook.pack(expand=True, fill="both")
+        # Frame Control
+        self.notebook = ttk.Frame(self.tabControl)
+
+        self.tabControl.add(self.tab1, text ='Duration')
+        self.tabControl.add(self.tab2, text ='Graphs') 
+        self.tabControl.pack(expand = 1, fill ="both")
+  
+    def center_window(self,w,h):
+        ws = self.root.winfo_screenwidth()
+        hs = self.root.winfo_screenheight()
+        x = (ws / 2) - (w / 2)
+        y = (hs / 2) - (h / 2)
+        self.root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+    # def _create_widgets(self):
+    #     self.notebook = ttk.Notebook(self.root)
+    #     processes = get_windows_processes(get_all_windows())
+    #     self.tree = create_process_tree(self.notebook, processes)
+    #     self.notebook.add(self.tree, text="Apps")
+    #     self.notebook.pack(expand=True, fill="both")
 
     def _update_tree(self):
-        """ Updates every second. """
         processes = get_windows_processes(get_all_windows())
-
-        for children in self.tree.get_children():
-            self.tree.delete(children)
-
+        for children in self.tab1.get_children():
+            self.tab1.delete(children)
         for process in processes:
-            self.tree.insert("", "end", values=process)
-
-        self.root.after(10000, self._update_tree)
+            self.tab1.insert("", "end", values=process)
+        self.root.after(10000, self._update_tree) # updates after 10 seconds, current refresh rate is about 17-23 seconds
 
     def start(self):
         self.root.after(1000, self._update_tree)
+        self.center_window(400, 700)
         self.root.mainloop()
 
 
 
 
-App().start()
-print(processes)
-print(windows)
+app().start()
+#print(processes)  // test output case1
+#print(windows)  // test output case2
