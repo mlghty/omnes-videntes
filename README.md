bc999736654a74bda98fbd3ed0df5413e329f546
CCHinkle <63796599+CCHinkle@users.noreply.github.com>
2020-11-29 08:03:19 -0600

    Add files via upload
    Added:
    - Login System
    - Registration System
    - Database Security
    
    To Add:
    -User Defined Time
    -Push Data Tab
    -Database/Graph Communication
    -SSL (With my current understanding, it will be difficult to distribute a program which uses this encryption, but it will be good to say we have)

--- README.md
+++ README.md
@@ -9,9 +9,6 @@ Built with Python 3.8, it lets you monitor and graph your running desktop apps.
 # To add
 - Grab foreground app pid's only
 - Grab the time delta between proccess time and current time / working but need to implement / display
-- Authenthication
-- Used id's
-- Encryption 
 - Graph(s)
 - Remove useless PID's
 - Duration selection
@@ -38,6 +35,8 @@ http://timgolden.me.uk/pywin32-docs/win32gui.html
 https://stackoverflow.com/questions/18470627/how-do-i-remove-the-microseconds-from-a-timedelta-object
 
 https://stackoverflow.com/questions/3352918/how-to-center-a-window-on-the-screen-in-tkinter
+https://codeshack.io/login-system-python-flask-mysql/
+https://www.percona.com/blog/2013/06/22/setting-up-mysql-ssl-and-secure-connections/
 
 ## Usage
 
--- kids/app/app.py
+++ kids/app/app.py
@@ -1,86 +1,48 @@
-import tkinter as tk
-from tkinter import ttk
-import time
-
+# main control file
+# 29 November, 2020
 from threading import Thread
 import threading
+import tkinter as tk
+from tkinter import ttk
 
-from kids.utils import get_all_windows,  get_windows_processes
-from kids.gui import create_process_tree
+from kids.gui import login_scr, register_scr, center_window
 
-# processes = get_windows_processes(get_all_windows()) //  case 1
-# windows = get_all_windows() // case 2
+class start_screen:
 
-
-class app:
     def __init__(self):
         self.root = tk.Tk()
-        self.root.title("I Over C") # Change name eventually
-        self.root.geometry("400x700") # Change window size or fix the expand fill for tab1
+        self.root.title("SK Authentication")
+        self.root.geometry("400x800")
         self.notebook()
-        #self._create_widgets()
-     
-    def notebook(self):
-        self.tabControl = ttk.Notebook(self.root)
-        processes = get_windows_processes(get_all_windows())
-        
-        # Tabs
-        self.tab1 = create_process_tree(self.tabControl, processes)
-        self.tab2 = ttk.Frame(self.tabControl)
 
-        # Style
+    def notebook(self):
+
+        # initializing tabs
+        self.tab_control = ttk.Notebook(self.root)
+        self.tab1 = ttk.Frame(self.tab_control)
+        self.tab2 = ttk.Frame(self.tab_control)
+        self.tab3 = ttk.Frame(self.tab_control)
+        self.tab4 = ttk.Frame(self.tab_control)
+
+        # notebook style
         noteStyler = ttk.Style()
         noteStyler.configure("TNotebook", background='white', borderwidth=0)
         noteStyler.configure("TNotebook.Tab", background='white')
         noteStyler.configure("TFrame", background='white')
 
         # Frame Control
-        self.notebook = ttk.Frame(self.tabControl)
+        self.notebook = ttk.Frame(self.tab_control)
+        self.tab_control.add(self.tab1, text ='Login')
+        self.tab_control.add(self.tab2, text ='Register')
+        self.tab_control.pack(expand = 1, fill ="both")
 
-        self.tabControl.add(self.tab1, text ='Duration')
-        self.tabControl.add(self.tab2, text ='Graphs') 
-        self.tabControl.pack(expand = 1, fill ="both")
-
-        # Labels
-        label = tk.Label(self.tab2, width=60)
-        label.configure(text="time remaining: %d seconds")        
-        label.place(x=0, y=650)
-  
-    def center_window(self,w,h):
-        ws = self.root.winfo_screenwidth()
-        hs = self.root.winfo_screenheight()
-        x = (ws / 2) - (w / 2)
-        y = (hs / 2) - (h / 2)
-        self.root.geometry('%dx%d+%d+%d' % (w, h, x, y))
-  
-    # Think lag comes from this function
-    def _update_tree(self):
-        processes = get_windows_processes(get_all_windows())
-        for children in self.tab1.get_children():
-            self.tab1.delete(children)
-        for process in processes:
-            self.tab1.insert("", "end", values=process)
-        
-        #self.root.after(10000, self._update_tree) # updates after 10 seconds, current refresh rate is about 17-23 seconds
-        self.t = Thread(target=self._update_tree)
-        self.t.setDaemon(True)
-        self.t.start()
+        # call login and register tabs
+        login_scr(self.tab1, self.tab3, self.tab4, self.tab_control, self.root).make_login()
+        register_scr(self.tab2, self.tab_control).make_register()
 
     def start(self):
-        #self.t = Thread(target=self._update_tree)
-        #self.t.setDaemon(True)
-        #self.t.start()
-
-        self.root.after(10000, self._update_tree)
-        #self.root.after(10000, self.t)
-        self.center_window(400, 700)
+        
+        center_window(self.root, 400, 450)
         self.root.mainloop()
 
-
-
-
-app().start()
-print(threading.activeCount())
-
-#print(processes)  // test output case1
-#print(windows)  // test output case2
+start_screen().start()
